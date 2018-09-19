7befe4fb868679d88e09cc908349591b1bf568a0
Dael <dael.separa@gmail.com>
2018-09-19 17:29:02 +0200

    change target platform to any

--- NeuralNetworkClassifier/gtk-gui/MainWindow.cs
+++ NeuralNetworkClassifier/gtk-gui/MainWindow.cs
@@ -239,7 +239,7 @@ public partial class MainWindow
 		this.MainNotebook.HeightRequest = 490;
 		this.MainNotebook.CanFocus = true;
 		this.MainNotebook.Name = "MainNotebook";
-		this.MainNotebook.CurrentPage = 3;
+		this.MainNotebook.CurrentPage = 2;
 		// Container child MainNotebook.Gtk.Notebook+NotebookChild
 		this.LayoutPageData = new global::Gtk.Fixed();
 		this.LayoutPageData.Name = "LayoutPageData";
